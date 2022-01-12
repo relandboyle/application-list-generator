0ef27f7623d110ea55b69604fe7d817dba5faad3
Reland Boyle <reland.boyle@gmail.com>
2022-01-12 01:40:53 -0500

    LinkedIn, Glass Door

--- generators.js
+++ generators.js
@@ -1,15 +1,15 @@
 /**
     linkedin.com
  */
-
-
+console.log([...new Set([...document.querySelectorAll('.job-card-container__company-name')].slice(4).map(nameDiv => nameDiv.innerText)).values()].join(','));
 
 
 /**
     glassdoor.com
  */
-
-
+console.log([...document.querySelectorAll('h2')].slice(1).reduce((csv, company) => {
+    return csv.concat(company.innerText, ',');
+}, ''));
 
 
 /**
