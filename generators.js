d038bad543873716b999e2d149a34a3a342ffd78
Reland Boyle <reland.boyle@gmail.com>
2022-04-02 11:17:25 -0400

    Bars

--- generators.js
+++ generators.js
@@ -1,37 +1,37 @@
 /**
     keyvalues.com
  */
-console.log([...document.querySelectorAll('.thumbnail-link .thumbnail-company')].reduce((csv, company) => csv.concat(company.innerText, ','), ''));
+console.log([...document.querySelectorAll('.thumbnail-link .thumbnail-company')].reduce((csv, company) => csv.concat(company.innerText, '|'), ''));
 
 
 /**
     linkedin.com
  */
-console.log([...new Set([...document.querySelectorAll('.job-card-container__company-name')].slice(4).map(nameDiv => nameDiv.innerText)).values()].join(','));
+console.log([...new Set([...document.querySelectorAll('.job-card-container__company-name')].slice(4).map(nameDiv => nameDiv.innerText)).values()].join('|'));
 
 
 /**
     glassdoor.com
  */
-console.log([...document.querySelectorAll('h2')].slice(1).reduce((csv, company) => csv.concat(company.innerText, ','), ''));
+console.log([...document.querySelectorAll('h2')].slice(1).reduce((csv, company) => csv.concat(company.innerText, '|'), ''));
 
 
 /**
     wealthfront.com
  */
-console.log([... document.getElementsByClassName('company_name')].map(div => div.innerText).join(','));
+console.log([... document.getElementsByClassName('company_name')].map(div => div.innerText).join('|'));
 
 
 /**
     techjobsforgood.com
  */
-console.log([...document.querySelectorAll('.company_name')].map((name) => name.innerText).join(','));
+console.log([...document.querySelectorAll('.company_name')].map((name) => name.innerText).join('|'));
 
 
 /**
     github.com/poteto/hiring-without-whiteboards
  */
-console.log([].concat(...[...document.getElementById('readme').querySelectorAll('ul')].slice(3, 12).map(list => [...list.querySelectorAll('a')].map(item => item.innerText))).join(','));
+console.log([].concat(...[...document.getElementById('readme').querySelectorAll('ul')].slice(3, 12).map(list => [...list.querySelectorAll('a')].map(item => item.innerText))).join('|'));
 
 
 /**
